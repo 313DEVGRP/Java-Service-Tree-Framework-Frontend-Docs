14df10a43deafb963c0163d3f4b338ae6f556d02
rolly <rollydream@hanmail.net>
2024-10-29 17:22:03 +0900

    컨텐츠 영역에서 '/' 시 나오는 메뉴 호출 안하는 부분들 삭제

--- packages/frontend/core/src/blocksuite/presets/ai/entries/slash-menu/setup-slash-menu.ts
+++ packages/frontend/core/src/blocksuite/presets/ai/entries/slash-menu/setup-slash-menu.ts
@@ -1,32 +1,14 @@
 import type {
-  AffineAIPanelWidget,
-  AffineSlashMenuActionItem,
   AffineSlashMenuContext,
-  AffineSlashMenuItem,
-  AffineSlashSubMenu,
   AIItemConfig,
 } from '@blocksuite/blocks';
 import {
   AFFINE_AI_PANEL_WIDGET,
   AffineSlashMenuWidget,
   AIStarIcon,
-  MoreHorizontalIcon,
 } from '@blocksuite/blocks';
-import { assertExists } from '@blocksuite/global/utils';
-import { html } from 'lit';
-
-import { AIItemGroups } from '../../_common/config';
-import { handleInlineAskAIAction } from '../../actions/doc-handler';
-import { AIProvider } from '../../provider';
 
 export function setupSlashMenuEntry(slashMenu: AffineSlashMenuWidget) {
-  const AIItems = AIItemGroups.map(group => group.items).flat();
-
-  const iconWrapper = (icon: AIItemConfig['icon']) => {
-    return html`<div style="color: var(--affine-primary-color)">
-      ${typeof icon === 'function' ? html`${icon()}` : icon}
-    </div>`;
-  };
 
   const showWhenWrapper =
     (item?: AIItemConfig) =>
@@ -45,38 +27,6 @@ export function setupSlashMenuEntry(slashMenu: AffineSlashMenuWidget) {
       return item?.showWhen?.(chain, editorMode, rootComponent.host) ?? true;
     };
 
-  const actionItemWrapper = (
-    item: AIItemConfig
-  ): AffineSlashMenuActionItem => ({
-    ...basicItemConfig(item),
-    action: ({ rootComponent }: AffineSlashMenuContext) => {
-      item?.handler?.(rootComponent.host);
-    },
-  });
-
-  const subMenuWrapper = (item: AIItemConfig): AffineSlashSubMenu => {
-    assertExists(item.subItem);
-    return {
-      ...basicItemConfig(item),
-      subMenu: item.subItem.map<AffineSlashMenuActionItem>(
-        ({ type, handler }) => ({
-          name: type,
-          action: ({ rootComponent }) => handler?.(rootComponent.host),
-        })
-      ),
-    };
-  };
-
-  const basicItemConfig = (item: AIItemConfig) => {
-    return {
-      name: item.name,
-      icon: iconWrapper(item.icon),
-      alias: ['ai'],
-      showWhen: showWhenWrapper(item),
-    };
-  };
-
-  
   const menu = slashMenu.config.items.slice();
   /* DrawIo 추가 */
   menu.unshift({
