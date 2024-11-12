06d5aeb194f1e197dcd14460ed674e6848116fde
313devops <313cokr@gmail.com>
2024-11-12 12:04:22 +0900

    feat: [KAN-99] #comment ✅ 오버라이드를 활용한 포맷바 수정 - 24.11.12 #close #time 1h +review SR @313devops 추신 : 맨앞에 붙일 수 있는 구분 •fix : Bug 류 처리 시 ( patch version up ) •feat : 신규 기능 류 처리 시 ( minor version up ) •perf : 메이저 기능 류 처리 시 ( major version up )
    코드리뷰는 코드를 통해 소통하고, 서로의 경험을 통해 배우면서 성장 할 수 있는 기회입니다.
    나를 위해, 나의 동료를 위해, 우리 팀을 위해 리뷰 시간을 사용해 주세요.
    •너무 많은 코드를 한꺼번에 리뷰하지 마세요.
    •최소한의 설명과 함께 중점적으로 봐야 할 곳을 알려 주세요.
    •리뷰 후 Comment를 남겨 주세요.
    •리뷰는 빠른 시간에 응답해 주세요.
    •지적 보다는 대안을 제시해 주세요.
    
    [ Frontend-DOC::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr:3010
    [Wiki] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [GitLab] http://www.313.co.kr/gitlab
    [Redmine] http://www.313.co.kr:3000/
    
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    
    [Docker Hub] https://hub.docker.com/u/313devgrp
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    [Kafka] http://www.313.co.kr/kafka
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/
    [DrawIO] http://www.313.co.kr/drawio
    [DrawDB] http://www.313.co.kr/drawdb

--- packages/frontend/core/src/blocksuite/presets/ai/entries/format-bar/setup-format-bar.ts
+++ packages/frontend/core/src/blocksuite/presets/ai/entries/format-bar/setup-format-bar.ts
@@ -1,9 +1,38 @@
 //import '../../_common/components/ask-ai-button';
 
+import type { CommandKeyToData } from '@blocksuite/block-std';
 import {
   type AffineFormatBarWidget,
-  toolbarDefaultConfig,
+  BoldIcon,
+  BulletedListIcon,
+  CheckBoxIcon,
+  CodeIcon,
+  DatabaseTableViewIcon20,
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  Heading4Icon,
+  Heading5Icon,
+  Heading6Icon,
+  ItalicIcon,
+  LinkedDocIcon,
+  LinkIcon,
+  NumberedListIcon,
+  QuoteIcon,
+  StrikethroughIcon,
+  TextIcon,
+  UnderlineIcon,
 } from '@blocksuite/blocks';
+import { createSimplePortal } from '@blocksuite/blocks/_common/components/portal';
+import { DATABASE_CONVERT_WHITE_LIST } from '@blocksuite/blocks/_common/configs/quick-action/database-convert-view';
+import {
+  convertSelectedBlocksToLinkedDoc,
+  getTitleFromSelectedModels,
+  notifyDocCreated,
+  promptDocTitle,
+} from '@blocksuite/blocks/_common/utils/render-linked-doc';
+import { assertExists } from '@blocksuite/global/utils';
+import { html } from 'lit';
 // import { html, type TemplateResult } from 'lit';
 
 // import { AIItemGroups } from '../../_common/config';
@@ -26,4 +55,231 @@ export function setupFormatBarEntry(formatBar: AffineFormatBarWidget) {
   //   ],
   //   0
   // );
+}
+
+export function toolbarDefaultConfig(toolbar: AffineFormatBarWidget) {
+  toolbar
+    .clearConfig()
+    .addParagraphDropdown()
+    .addDivider()
+    .addTextStyleToggle({
+      key: 'bold',
+      action: chain => chain.toggleBold().run(),
+      icon: BoldIcon,
+    })
+    .addTextStyleToggle({
+      key: 'italic',
+      action: chain => chain.toggleItalic().run(),
+      icon: ItalicIcon,
+    })
+    .addTextStyleToggle({
+      key: 'underline',
+      action: chain => chain.toggleUnderline().run(),
+      icon: UnderlineIcon,
+    })
+    .addTextStyleToggle({
+      key: 'strike',
+      action: chain => chain.toggleStrike().run(),
+      icon: StrikethroughIcon,
+    })
+    .addTextStyleToggle({
+      key: 'code',
+      action: chain => chain.toggleCode().run(),
+      icon: CodeIcon,
+    })
+    .addTextStyleToggle({
+      key: 'link',
+      action: chain => chain.toggleLink().run(),
+      icon: LinkIcon,
+    })
+    .addDivider()
+    .addHighlighterDropdown()
+    .addDivider()
+    .addInlineAction({
+      id: 'convert-to-database',
+      name: 'Create Database',
+      icon: DatabaseTableViewIcon20,
+      isActive: () => false,
+      action: () => {
+        createSimplePortal({
+          template: html`<database-convert-view
+            .host=${toolbar.host}
+          ></database-convert-view>`,
+        });
+      },
+      showWhen: chain => {
+        const middleware = (count = 0) => {
+          return (
+            ctx: CommandKeyToData<'selectedBlocks'>,
+            next: () => void
+          ) => {
+            const { selectedBlocks } = ctx;
+            if (!selectedBlocks || selectedBlocks.length === count) return;
+
+            const allowed = selectedBlocks.every(block =>
+              DATABASE_CONVERT_WHITE_LIST.includes(block.flavour)
+            );
+            if (!allowed) return;
+
+            next();
+          };
+        };
+        let [result] = chain
+          .getTextSelection()
+          .getSelectedBlocks({
+            types: ['text'],
+          })
+          .inline(middleware(1))
+          .run();
+
+        if (result) return true;
+
+        [result] = chain
+          .tryAll(chain => [
+            chain.getBlockSelections(),
+            chain.getImageSelections(),
+          ])
+          .getSelectedBlocks({
+            types: ['block', 'image'],
+          })
+          .inline(middleware(0))
+          .run();
+
+        return result;
+      },
+    })
+    .addDivider()
+    .addInlineAction({
+      id: 'convert-to-linked-doc',
+      name: 'Create Linked Doc 이게 바로 오버라이드',
+      icon: LinkedDocIcon,
+      isActive: () => false,
+      action: (chain, formatBar) => {
+        const [_, ctx] = chain
+          .getSelectedModels({
+            types: ['block', 'text'],
+            mode: 'highest',
+          })
+          .run();
+        const { selectedModels } = ctx;
+        assertExists(selectedModels);
+        if (!selectedModels.length) return;
+
+        const host = formatBar.host;
+        host.selection.clear();
+
+        const doc = host.doc;
+        const autofill = getTitleFromSelectedModels(selectedModels);
+        void promptDocTitle(host, autofill).then(title => {
+          if (title === null) return;
+          const linkedDoc = convertSelectedBlocksToLinkedDoc(
+            doc,
+            selectedModels,
+            title
+          );
+          const linkedDocService = host.spec.getService(
+            'affine:embed-linked-doc'
+          );
+          linkedDocService.slots.linkedDocCreated.emit({ docId: linkedDoc.id });
+          notifyDocCreated(host, doc);
+          host.spec
+            .getService('affine:page')
+            .telemetryService?.track('DocCreated', {
+              control: 'create linked doc',
+              page: 'doc editor',
+              module: 'format toolbar',
+              type: 'embed-linked-doc',
+            });
+          host.spec
+            .getService('affine:page')
+            .telemetryService?.track('LinkedDocCreated', {
+              control: 'create linked doc',
+              page: 'doc editor',
+              module: 'format toolbar',
+              type: 'embed-linked-doc',
+            });
+        });
+      },
+      showWhen: chain => {
+        const [_, ctx] = chain
+          .getSelectedModels({
+            types: ['block', 'text'],
+            mode: 'highest',
+          })
+          .run();
+        const { selectedModels } = ctx;
+        return !!selectedModels && selectedModels.length > 0;
+      },
+    })
+    .addBlockTypeSwitch({
+      flavour: 'affine:paragraph',
+      type: 'text',
+      name: 'Text',
+      icon: TextIcon,
+    })
+    .addBlockTypeSwitch({
+      flavour: 'affine:paragraph',
+      type: 'h1',
+      name: 'Heading 1',
+      icon: Heading1Icon,
+    })
+    .addBlockTypeSwitch({
+      flavour: 'affine:paragraph',
+      type: 'h2',
+      name: 'Heading 2',
+      icon: Heading2Icon,
+    })
+    .addBlockTypeSwitch({
+      flavour: 'affine:paragraph',
+      type: 'h3',
+      name: 'Heading 3',
+      icon: Heading3Icon,
+    })
+    .addBlockTypeSwitch({
+      flavour: 'affine:paragraph',
+      type: 'h4',
+      name: 'Heading 4',
+      icon: Heading4Icon,
+    })
+    .addBlockTypeSwitch({
+      flavour: 'affine:paragraph',
+      type: 'h5',
+      name: 'Heading 5',
+      icon: Heading5Icon,
+    })
+    .addBlockTypeSwitch({
+      flavour: 'affine:paragraph',
+      type: 'h6',
+      name: 'Heading 6',
+      icon: Heading6Icon,
+    })
+    .addBlockTypeSwitch({
+      flavour: 'affine:list',
+      type: 'bulleted',
+      name: 'Bulleted List',
+      icon: BulletedListIcon,
+    })
+    .addBlockTypeSwitch({
+      flavour: 'affine:list',
+      type: 'numbered',
+      name: 'Numbered List',
+      icon: NumberedListIcon,
+    })
+    .addBlockTypeSwitch({
+      flavour: 'affine:list',
+      type: 'todo',
+      name: 'To-do List',
+      icon: CheckBoxIcon,
+    })
+    .addBlockTypeSwitch({
+      flavour: 'affine:code',
+      name: 'Code Block',
+      icon: CodeIcon,
+    })
+    .addBlockTypeSwitch({
+      flavour: 'affine:paragraph',
+      type: 'quote',
+      name: 'Quote',
+      icon: QuoteIcon,
+    });
 }
