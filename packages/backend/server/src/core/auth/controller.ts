c101cc94614733a8650d45b20e900106b372f140
313devops <313cokr@gmail.com>
2024-11-19 15:00:59 +0900

    feat: [KAN-99] #comment ✅ Server API 구성 : 로컬 빌드 - 24.11.19 #close #time 1h +review SR @313devops
    추신 : 맨앞에 붙일 수 있는 구분
    •fix : Bug 류 처리 시 ( patch version up )
    •feat : 신규 기능 류 처리 시 ( minor version up )
    •perf : 메이저 기능 류 처리 시 ( major version up )
    
    코드리뷰는 코드를 통해 소통하고, 서로의 경험을 통해 배우면서 성장 할 수 있는 기회입니다.
    나를 위해, 나의 동료를 위해, 우리 팀을 위해 리뷰 시간을 사용해 주세요.
    •너무 많은 코드를 한꺼번에 리뷰하지 마세요.
    •최소한의 설명과 함께 중점적으로 봐야 할 곳을 알려 주세요.
    •리뷰 후 Comment를 남겨 주세요.
    •리뷰는 빠른 시간에 응답해 주세요.
    •지적 보다는 대안을 제시해 주세요.
    
    [ Frontend-DOC::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr:3010
    [Wiki] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [GitLab] http://www.313.co.kr/gitlab
    [Redmine] http://www.313.co.kr:3000/
    
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    
    [Docker Hub] https://hub.docker.com/u/313devgrp
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    [Kafka] http://www.313.co.kr/kafka
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/
    [DrawIO] http://www.313.co.kr/drawio
    [DrawDB] http://www.313.co.kr/drawdb

--- packages/backend/server/src/core/auth/controller.ts
+++ packages/backend/server/src/core/auth/controller.ts
@@ -35,6 +35,33 @@ class SignInCredential {
   password?: string;
 }
 
+class ReqAdd {
+  ref?: number;
+  c_title?: string;
+  c_type?: string;
+  c_req_pdservice_link?: string;
+  c_req_pdservice_versionset_link?: string;
+  c_req_start_date?: string;
+  c_req_end_date?: string;
+  c_req_writer?: string;
+  c_req_contents?: string;
+  c_req_desc?: string;
+  c_req_etc?: string;
+  c_req_priority_link?: string;
+  c_req_difficulty_link?: string;
+  c_req_state_link?: string;
+  c_req_reviewer01?: string;
+  c_req_reviewer02?: string;
+  c_req_reviewer03?: string;
+  c_req_reviewer04?: string;
+  c_req_reviewer05?: string;
+  c_req_reviewer01_status?: string;
+  c_req_reviewer02_status?: string;
+  c_req_reviewer03_status?: string;
+  c_req_reviewer04_status?: string;
+  c_req_reviewer05_status?: string;
+}
+
 class MagicLinkCredential {
   email!: string;
   token!: string;
@@ -210,13 +237,43 @@ export class AuthController {
   }
 
   @Public()
-  @Get('/arms')
+  @Post('/arms')
+  @Header('content-type', 'application/json')
   async arms(
+    @Req() req: Request,
+    @Res() res: Response,
     @CurrentUser() user?: CurrentUser
   ) {
     // ARMS API
     return {
-      user: user
+      req: req,
+      res: res,
+      user: user,
+      arms_ref: reqadd.ref,
+      arms_c_title: reqadd.c_title,
+      arms_c_type: reqadd.c_type,
+      arms_c_req_pdservice_link: reqadd.c_req_pdservice_link,
+      arms_c_req_pdservice_versionset_link:
+        reqadd.c_req_pdservice_versionset_link,
+      arms_c_req_start_date: reqadd.c_req_start_date,
+      arms_c_req_end_date: reqadd.c_req_end_date,
+      arms_c_req_writer: reqadd.c_req_writer,
+      arms_c_req_contents: reqadd.c_req_contents,
+      arms_c_req_desc: reqadd.c_req_desc,
+      arms_c_req_etc: reqadd.c_req_etc,
+      arms_c_req_priority_link: reqadd.c_req_priority_link,
+      arms_c_req_difficulty_link: reqadd.c_req_difficulty_link,
+      arms_c_req_state_link: reqadd.c_req_state_link,
+      arms_c_req_reviewer01: reqadd.c_req_reviewer01,
+      arms_c_req_reviewer02: reqadd.c_req_reviewer02,
+      arms_c_req_reviewer03: reqadd.c_req_reviewer03,
+      arms_c_req_reviewer04: reqadd.c_req_reviewer04,
+      arms_c_req_reviewer05: reqadd.c_req_reviewer05,
+      arms_c_req_reviewer01_status: reqadd.c_req_reviewer01_status,
+      arms_c_req_reviewer02_status: reqadd.c_req_reviewer02_status,
+      arms_c_req_reviewer03_status: reqadd.c_req_reviewer03_status,
+      arms_c_req_reviewer04_status: reqadd.c_req_reviewer04_status,
+      arms_c_req_reviewer05_status: reqadd.c_req_reviewer05_status,
     };
   }
 }
