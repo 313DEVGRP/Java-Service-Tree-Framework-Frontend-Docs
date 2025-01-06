d35f38828d53ec1704cbf14960722b637130a3d0
rolly <rollydream@hanmail.net>
2025-01-06 16:34:06 +0900

    Multiselect 옵션 테스트 수정

--- packages/frontend/core/src/components/blocksuite/block-suite-editor/specs/custom/spec-patchers.tsx
+++ packages/frontend/core/src/components/blocksuite/block-suite-editor/specs/custom/spec-patchers.tsx
@@ -123,12 +123,19 @@ async function fetchOptions() {
     //TODO : 내부 Backend 로 API 호출 결과를.
     //TODO : 멀티 셀렉트에 데이터를 바인딩 하고 싶습니다. ( c_title 을 바인등 하면 되고 : key, value 는 c_id 입니다. )
     const productResponse = await axios.get('/api/auth/arms/pdService');
-    //const productResponse = [{key: 'Option 1️⃣', value: 1},{key: 'Option 2️⃣', value: 2}];
-    //const versionResponse = await axios.get('/api/auth/arms/version');
-    const versionResponse = [{key: 'Option 11️⃣', value: 1},{key: 'Option 22️⃣', value: 2}];
+    const productOptions = productResponse.data.map((item: { c_title: any; c_id: any; }) => ({
+      key: item.c_title,
+      value: item.c_id
+    }));
+    const versionResponse = await axios.get('/api/auth/arms/version');
+    const versionOptions = versionResponse.data.map((item: { c_title: any; c_id: any; }) => ({
+      key: item.c_title,
+      value: item.c_id
+    }));
+
     return {
-      productOptions: productResponse,
-      versionOptions: versionResponse,
+      productOptions,
+      versionOptions
     };
   } catch (error) {
     console.error('Failed to fetch options:', error);
@@ -187,6 +194,8 @@ export function patchNotificationService(
 
         // 데이터 로드
         const { productOptions, versionOptions } = await fetchOptions();
+
+        console.log(productOptions, versionOptions);
 
         return new Promise<string | null>(resolve => {
           let value = autofill || '';
