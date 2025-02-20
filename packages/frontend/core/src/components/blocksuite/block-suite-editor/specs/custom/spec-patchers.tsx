c132166c4d6a10b8849ddd89867fd18387fa7ad1
313devops <313cokr@gmail.com>
2025-02-20 15:44:03 +0900

    feat: [KAN-99] #comment ✅ 한꺼번에 로딩해서 프론트에서 처리하는 방안 모색 ( 초기화 베이스 코드 ) - 25.02.20 #close #time 1h +review SR @313devops
    추신 : 맨앞에 붙일 수 있는 구분
    •fix : Bug 류 처리 시 ( patch version up )
    •feat : 신규 기능 류 처리 시 ( minor version up )
    •perf : 메이저 기능 류 처리 시 ( major version up )
    
    코드리뷰는 코드를 통해 소통하고, 서로의 경험을 통해 배우면서 성장 할 수 있는 기회입니다.
    나를 위해, 나의 동료를 위해, 우리 팀을 위해 리뷰 시간을 사용해 주세요.
    •너무 많은 코드를 한꺼번에 리뷰하지 마세요.
    •최소한의 설명과 함께 중점적으로 봐야 할 곳을 알려 주세요.
    •리뷰 후 Comment를 남겨 주세요.
    •리뷰는 빠른 시간에 응답해 주세요.
    •지적 보다는 대안을 제시해 주세요.
    
    [ Frontend-DOC::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr:3010
    [Wiki] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [GitLab] http://www.313.co.kr/gitlab
    [Redmine] http://www.313.co.kr:3000/
    
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    
    [Docker Hub] https://hub.docker.com/u/313devgrp
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    [Kafka] http://www.313.co.kr/kafka
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/
    [DrawIO] http://www.313.co.kr/drawio
    [DrawDB] http://www.313.co.kr/drawdb

--- packages/frontend/core/src/components/blocksuite/block-suite-editor/specs/custom/spec-patchers.tsx
+++ packages/frontend/core/src/components/blocksuite/block-suite-editor/specs/custom/spec-patchers.tsx
@@ -144,7 +144,7 @@ async function fetchVersionOptions(selectedProduct?:number) {
     const versionResponse = await axios.get('/api/auth/version?c_req_pdservice='+selectedProduct);
     // 응답 데이터에서 필요한 부분 추출
     const versionOptions =
-      versionResponse.data?.result?.map(
+      versionResponse.data?.result?.response?.map(
         (item: { c_id: number; c_title: string }) => ({
           key: item.c_title, // value는 c_title (멀티 셀렉트에서 보여질 값)
           value: item.c_id, // key는 c_id
