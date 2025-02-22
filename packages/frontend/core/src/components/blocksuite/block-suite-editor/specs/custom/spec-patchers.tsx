21c738cf0966e5a6e0c209a9c2fd3df4e9376562
313cokr@gmail.com <313cokr@gmail.com>
2025-02-22 17:52:00 +0900

    spec 확인

--- packages/frontend/core/src/components/blocksuite/block-suite-editor/specs/custom/spec-patchers.tsx
+++ packages/frontend/core/src/components/blocksuite/block-suite-editor/specs/custom/spec-patchers.tsx
@@ -115,6 +115,10 @@ export function patchReferenceRenderer(
       );
     }
 
+    console.log("spec return --------------- start");
+    console.log(spec);
+    console.log("spec return --------------- end");
+
     return spec;
   });
 }
